2dfc2d99039353121fe296a20d059fe3d6beda08
mdalaminfaraji <alaminice1617@gmail.com>
2023-12-01 12:11:05 +0600

    project done

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('meals/',include('meal.urls') ),
     path('about/',include('about_us.urls') )
 ]
